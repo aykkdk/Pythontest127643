f83f234328ee711a9f3489a8941a454a7f14a711
Elysia <117100940+aykkdk@users.noreply.github.com>
2025-01-25 08:59:57 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# hello.py
+#this is a test
+def greet(name):
+    return f"Hello, {name}!"
+
+# Test the function
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet(name))
+    
+    
+   #test
+   #test
